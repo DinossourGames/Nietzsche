f408802cb2c2c3ccb9c6e465dc8963934c1b5a02
Felipe Alef <alef@dinossour.com.br>
2019-09-06 01:45:15 -0300

    Player recognize touchs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,34 @@
-﻿using System.Runtime.Serialization;
+﻿using System;
+using System.Runtime.Serialization;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
     [SerializeField] private float speed;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
 
-    private void Awake()
-    {
-        SwipeDetector.OnSwipe += SwipeDetectorOnOnSwipe;
-    }
-
-    private void SwipeDetectorOnOnSwipe(SwipeData obj)
-    {
-        _rigidbody2D.velocity = obj.VectorDirection * speed;
-    }
+    private Vector2 _size;
 
     void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _size = _spriteRenderer.bounds.extents;
+    }
+
+    private void Update()
+    {
+        foreach (var touch in Input.touches)
+        {
+            Vector2 poz = Camera.main.ScreenToWorldPoint(touch.position);
+
+            if (poz.x > transform.position.x - _size.x && poz.x < transform.position.x + _size.x &&
+                poz.y > transform.position.y - _size.y && poz.y < transform.position.y + _size.y)
+            {
+                if (touch.phase == TouchPhase.Ended)
+                    Debug.Log("Hit");
+            }
+        }
     }
 }
